4edff4fdf1bc858bfea3f56678e8b3cda74581ca
Dillon Chanis <dillonchanis92@gmail.com>
2016-10-22 14:13:21 -0400

    Creating Achievements component to prepare for Achievement system.

---
+++ src/components/Achievements.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Achievements extends React.Component {
+	render() {
+		return(
+			<div>
+				<h2>Achievements</h2>
+			</div>
+		);
+	}
+};
+
+export default Achievements;
+
